de2f5d2224724364c95d8ce3a723ba7562a287cc
Stephen <stearsail@gmail.com>
2026-09-09 11:13:14 +0300

    mcp: helper function to get servers (will be more afterwards), function to build toolkits list[MCPTools] for use with agent

--- src/assistant/tools/servers.py
+++ src/assistant/tools/servers.py
@@ -1,11 +1,25 @@
-import os
+import assistant.config as config
+from agno.tools.mcp import MCPTools
 
-SERVERS = {
-    "google_workspace": {
-        "command": "uvx workspace-mcp --tools calendar tasks gmail docs",
-        "env": {
-            "GOOGLE_OAUTH_CLIENT_ID": os.environ["GOOGLE_OAUTH_CLIENT_ID"],
-            "GOOGLE_OAUTH_CLIENT_SECRET": os.environ["GOOGLE_OAUTH_CLIENT_SECRET"],
+def _get_servers() -> dict:
+    creds = config.load_credentials()
+    return {
+        "google_workspace": {
+            "command": "uvx workspace-mcp --tools calendar tasks gmail docs",
+            "env": {
+                "GOOGLE_OAUTH_CLIENT_ID": creds["client_id"],
+                "GOOGLE_OAUTH_CLIENT_SECRET": creds["client_secret"],
+            },
         },
-    },
-}
+    }
+
+
+def build_toolkits() -> list[MCPTools]:
+    server = _get_servers()
+    toolkits = []
+    google_workspace = MCPTools(
+        command=server["google_workspace"]["command"],
+        env=server["google_workspace"]["env"],
+    )
+    toolkits.append(google_workspace)
+    return toolkits
